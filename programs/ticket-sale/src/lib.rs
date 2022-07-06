78b990abb3673b3775ba5b500e30b5d220586569
ppoliani <ppoliani@gmail.com>
2022-07-06 11:59:58 +0300

    chore: setup the ticket sale program

---
+++ programs/ticket-sale/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6banhWF9WKQk26NtgX6TqHNmAKgvX9aJmgaBvPmYCCK3");
+
+#[program]
+pub mod ticket_sale {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
