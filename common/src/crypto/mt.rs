5ab5f733d4f8a223d463d080777047c9e2ab871c
ppoliani <ppoliani@gmail.com>
2022-07-06 11:53:31 +0300

    feat: add a common crate

--- common/src/crypto/mt.rs
+++ common/src/crypto/mt.rs
@@ -1,10 +1,8 @@
-use anchor_lang::prelude::*;
+use anyhow::Result;
 use anchor_lang::{
   solana_program::keccak::hashv,
 };
-use crate::{
-  program_error::{ErrorCode},
-};
+use crate::error::CommonError;
 
 fn verify(proof: Vec<[u8; 32]>, root: [u8; 32], leaf: [u8; 32]) -> bool {
   let mut computed_hash = leaf;
@@ -25,17 +23,12 @@ fn verify(proof: Vec<[u8; 32]>, root: [u8; 32], leaf: [u8; 32]) -> bool {
 
 pub fn can_mint<'info>(
   merkle_proof: Vec<[u8; 32]>,
-  token_id: u32,
-  name: u32,
   merkle_root: [u8; 32],
-  user: &Signer<'info>,
+  leaf: [u8; 32],
 ) -> Result<()> {
-  let leaf = hashv(&[
-    user.key().as_ref(),
-    token_id.to_string().as_ref(),
-    name.to_string().as_ref()
-  ]).0;
-  require!(verify(merkle_proof, merkle_root, leaf), ErrorCode::InvalidMerkleProof);
+  if !verify(merkle_proof, merkle_root, leaf) {
+    return Err(CommonError::InvalidProof.into())
+  }
   
   Ok(())
 }
