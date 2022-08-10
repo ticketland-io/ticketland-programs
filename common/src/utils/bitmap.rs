449fdbb7cd5223d45a04f4b92b0ab6b60edb4e50
ppoliani <ppoliani@gmail.com>
2022-08-10 18:45:52 +0300

    feat(common): temporarily use spl math

--- common/src/utils/bitmap.rs
+++ common/src/utils/bitmap.rs
@@ -1,5 +1,15 @@
+use spl_math::precise_number::PreciseNumber;
+
 fn index_to_byte_and_bit(index: u32) -> (usize, usize, usize) {
-	let byte = (index as f64 / 8_f64).floor() as usize;
+  let byte = PreciseNumber::new(index.into())
+  .unwrap()
+  .checked_div(&PreciseNumber::new(8).unwrap())
+  .unwrap()
+  .floor()
+  .unwrap()
+  .to_imprecise()
+  .unwrap() as usize;
+
 	let bit = index as usize - (byte * 8); // instead of using module index % 8
   let mask = 1 << bit;
 
