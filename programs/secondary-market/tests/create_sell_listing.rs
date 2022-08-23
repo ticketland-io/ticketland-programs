58f248954796d9c5f2ae8d266501db91bda5e300
ppoliani <ppoliani@gmail.com>
2022-08-23 14:16:39 +0300

    test(secondary-market): test than wron sale account cannot be used in create sell listing

--- programs/secondary-market/tests/create_sell_listing.rs
+++ programs/secondary-market/tests/create_sell_listing.rs
@@ -1,37 +1,24 @@
 #![cfg(feature = "test-bpf")]
-use anchor_lang::{
-  prelude::{
-    Pubkey,
-  },
-};
 use test_context::{test_context, futures};
 use solana_sdk::{
-  signature::{Signer, Keypair},
+  signature::{Signer},
   native_token::sol_to_lamports,
 };
 use solana_program_test::{tokio};
-use common::{
-  state::{
-    ticket_type::{TicketType, SeatRange},
-    sale_type::SaleType,
-  },
-};
 use common_test::{
   test_context::TestContext,
   ticket_sale::{
-    runner::Runner as TicketSaleRunner,
-    pda::{self as TicketSalePda, ticket_sale_state},
+    pda::{self as TicketSalePda},
   },
   secondary_market::{
     common::{init, setup},
     error::Error,
   }
 };
-use ticket_sale::account_data::event;
 
 #[test_context(TestContext)]
 #[tokio::test(flavor = "multi_thread")]
-async fn should_fail_if_sale_finished(ctx: &mut TestContext) {
+async fn should_enforce_access_control(ctx: &mut TestContext) {
   let (
     event_registry_state,
     ticket_sale_state,
@@ -62,17 +49,78 @@ async fn should_fail_if_sale_finished(ctx: &mut TestContext) {
     ticket_type_index,
   ).await;
 
-  let secondary_market_runner = &mut ctx.secondary_market_runner;
+  {
+    let secondary_market_runner = &mut ctx.secondary_market_runner;
+    
+    let result = secondary_market_runner.create_market(
+      secondary_market_state.pubkey(),
+      event_registry_state.pubkey(),
+      event_id,
+      &event_organizer,
+      500, // organizer_resale_fee 5%
+      1000, // resale_cap 10%
+    ).await;
+    assert!(result.is_ok());
+  }
   
-  let result = secondary_market_runner.create_market(
-    secondary_market_state.pubkey(),
-    event_registry_state.pubkey(),
-    event_id,
-    &event_organizer,
-    500, // organizer_resale_fee 5%
-    1000, // resale_cap 10%
-  ).await;
-  assert!(result.is_ok());
+  // should fail if wrong sale account is passed
+  {
+    let some_other_event_id: [u8; 32] = "aaaa6394e04a4b3c8ccd7e2772cb14b4".to_owned().into_bytes().try_into().unwrap();
+    
+    {
+      // create a new event
+      let _ = setup(
+        ctx,
+        &event_organizer,
+        &ticket_buyer,
+        event_registry_state.pubkey(),
+        ticket_sale_state.pubkey(),
+        ticket_nft_state.pubkey(),
+        deposit_token,
+        purchase_token,
+        some_other_event_id,
+        seat_index,
+        ticket_type_index,
+      ).await;
+
+      let secondary_market_runner = &mut ctx.secondary_market_runner;
+
+      // create a market for that sale
+      let result = secondary_market_runner.create_market(
+        secondary_market_state.pubkey(),
+        event_registry_state.pubkey(),
+        some_other_event_id,
+        &event_organizer,
+        500, // organizer_resale_fee 5%
+        1000, // resale_cap 10%
+      ).await;
+      assert!(result.is_ok());
+    }
+
+    // now use this new sale to create listing for a ticket that was purchased in the previous sale
+    let sale = TicketSalePda::ticket_sale_state(
+      &ticket_sale_state.pubkey(),
+      ticket_type_index,
+      some_other_event_id,
+    ).0;
+  
+    let secondary_market_runner = &mut ctx.secondary_market_runner;
+    let result = secondary_market_runner.create_sell_listing(
+      event_id,
+      // 4% higher than the price sold in the primary market.
+      // cap is at 5%
+      sol_to_lamports(1.04),
+      secondary_market_state.pubkey(),
+      event_registry_state.pubkey(),
+      sale,
+      seat_index,
+      ticket_nft_state.pubkey(),
+      purchase_token,
+      &ticket_buyer,
+    ).await;
+  
+    Error::assert_err(result, secondary_market::utils::program_error::ErrorCode::WrongSaleAccount);
+  }
 
   // move to the end of sale
   {
@@ -81,25 +129,30 @@ async fn should_fail_if_sale_finished(ctx: &mut TestContext) {
     pt.advance_clock_past_timestamp(ticket_types[0].sale_end_time + 1).await;
   }
 
-  let sale = TicketSalePda::ticket_sale_state(
-    &ticket_sale_state.pubkey(),
-    ticket_type_index,
-    event_id,
-  ).0;
+  // Should fail if ticket sale is finished
+  {
+    let secondary_market_runner = &mut ctx.secondary_market_runner;
 
-  let result = secondary_market_runner.create_sell_listing(
-    event_id,
-    // 4% higher than the price sold in the primary market.
-    // cap is at 5%
-    sol_to_lamports(1.04),
-    secondary_market_state.pubkey(),
-    event_registry_state.pubkey(),
-    sale,
-    seat_index,
-    ticket_nft_state.pubkey(),
-    purchase_token,
-    &ticket_buyer,
-  ).await;
+    let sale = TicketSalePda::ticket_sale_state(
+      &ticket_sale_state.pubkey(),
+      ticket_type_index,
+      event_id,
+    ).0;
 
-  Error::assert_ticket_sale_err(result, ticket_sale::utils::program_error::ErrorCode::SaleFinished);
+    let result = secondary_market_runner.create_sell_listing(
+      event_id,
+      // 4% higher than the price sold in the primary market.
+      // cap is at 5%
+      sol_to_lamports(1.04),
+      secondary_market_state.pubkey(),
+      event_registry_state.pubkey(),
+      sale,
+      seat_index,
+      ticket_nft_state.pubkey(),
+      purchase_token,
+      &ticket_buyer,
+    ).await;
+
+    Error::assert_ticket_sale_err(result, ticket_sale::utils::program_error::ErrorCode::SaleFinished);
+  }
 }
