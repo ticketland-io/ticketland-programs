992259ad06432e34de5a0b4f42334146f33269f9
ppoliani <ppoliani@gmail.com>
2022-08-23 13:17:27 +0300

    test(secondary-market): should_only_be_called_by_the_event_organizer

--- programs/secondary-market/tests/create_market.rs
+++ programs/secondary-market/tests/create_market.rs
@@ -9,6 +9,7 @@ use common_test::{
   secondary_market::{
     common::{init, setup},
     pda,
+    error::Error,
   }
 };
 
@@ -77,33 +78,41 @@ async fn should_only_be_called_by_the_event_organizer(ctx: &mut TestContext) {
 
   let event_id: [u8; 32] = "85ac6394e04a4b3c8ccd7e2772cb14b4".to_owned().into_bytes().try_into().unwrap();
   let seat_index = 0;
-  let event_registry_runner = &mut ctx.event_registry_runner;
-  let event_organizer = event_registry_runner.get_participant(1);
-  let ticket_buyer = event_registry_runner.get_participant(2);
-  let deposit_token = event_registry_runner.deposit_tokens[2];
-  let purchase_token = deposit_token;
 
-  let _ = setup(
-    ctx,
-    &event_organizer,
-    &ticket_buyer,
-    event_registry_state.pubkey(),
-    ticket_sale_state.pubkey(),
-    ticket_nft_state.pubkey(),
-    deposit_token,
-    purchase_token,
-    event_id,
-    seat_index,
-  ).await;
+  {
+    let event_registry_runner = &mut ctx.event_registry_runner;
+    let event_organizer = event_registry_runner.get_participant(1);
+    let ticket_buyer = event_registry_runner.get_participant(2);
+    let deposit_token = event_registry_runner.deposit_tokens[2];
+    let purchase_token = deposit_token;
 
-  let wrong_event_organizer = event_registry_runner.get_participant(3);
-  let secondary_market_runner = &mut ctx.secondary_market_runner;
-  let result = secondary_market_runner.create_market(
-    secondary_market_state.pubkey(),
-    event_registry_state.pubkey(),
-    event_id,
-    &wrong_event_organizer,
-    500, // organizer_resale_fee 5%
-    1000, // resale_cap 10%
-  ).await;
+    let _ = setup(
+      ctx,
+      &event_organizer,
+      &ticket_buyer,
+      event_registry_state.pubkey(),
+      ticket_sale_state.pubkey(),
+      ticket_nft_state.pubkey(),
+      deposit_token,
+      purchase_token,
+      event_id,
+      seat_index,
+    ).await;
+  }
+
+  {
+    let event_registry_runner = &mut ctx.event_registry_runner;
+    let wrong_event_organizer = event_registry_runner.get_participant(3);
+    let secondary_market_runner = &mut ctx.secondary_market_runner;
+    let result = secondary_market_runner.create_market(
+      secondary_market_state.pubkey(),
+      event_registry_state.pubkey(),
+      event_id,
+      &wrong_event_organizer,
+      500, // organizer_resale_fee 5%
+      1000, // resale_cap 10%
+    ).await;
+
+    Error::assert_err(result, secondary_market::utils::program_error::ErrorCode::OnlyEventOrganizer);
+  }
 }
