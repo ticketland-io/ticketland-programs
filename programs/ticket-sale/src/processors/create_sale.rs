bc8c344038be6fde4f35c98ec39da5fd1b9c780c
ppoliani <ppoliani@gmail.com>
2022-09-03 13:42:25 +0300

    feat(ticket-sale): store ticket type in the sale account

--- programs/ticket-sale/src/processors/create_sale.rs
+++ programs/ticket-sale/src/processors/create_sale.rs
@@ -1,8 +1,5 @@
 use anchor_lang::prelude::*;
 use common::{
-	state::{
-    ticket_type::TicketType,
-  },
   account_data::{
     serialization::deser,
   },
@@ -17,9 +14,7 @@ use crate::{
   }
 };
 
-fn event_account_checks(ctx: &Context<CreateSale>, ticket_type_index: u8,) -> Result<()> {
-  let event: Event = deser(ctx.accounts.event.clone())?;
-
+fn event_account_checks(ctx: &Context<CreateSale>, event: &Event, ticket_type_index: u8,) -> Result<()> {
   // The provided event organizer account should be the same as the one stored in the event
   require!(ctx.accounts.event_organizer.key() == event.event_organizer, ErrorCode::WrongEventOrganizer);
   require!(ticket_type_index < event.ticket_types.len() as u8, ErrorCode::InvalidTicketTypeIndex);
@@ -31,10 +26,11 @@ pub fn exec(
   ctx: Context<CreateSale>,
   ticket_type_index: u8,
   event_id: [u8; 32],
-  ticket_type: TicketType,
 ) -> Result<()> {
-  event_account_checks(&ctx, ticket_type_index)?;
+  let event: Event = deser(ctx.accounts.event.clone())?;
+  event_account_checks(&ctx, &event, ticket_type_index)?;
 
+  let ticket_type = event.ticket_types[ticket_type_index as usize];
   let sale = &mut ctx.accounts.sale;
 
   sale.bump = *ctx.bumps.get("sale").unwrap();
