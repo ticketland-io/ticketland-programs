54873510ec38cc1827210d7515cd2695a48e51ce
ppoliani <ppoliani@gmail.com>
2022-10-15 14:31:54 +0300

    test(ticket-sale): add operator_purchase to the runner

--- common-test/src/ticket_sale/runner.rs
+++ common-test/src/ticket_sale/runner.rs
@@ -329,4 +329,58 @@ impl Runner {
 
     self.process_transaction(&[ix], Some(&[&ticket_buyer])).await
   }
+
+  pub async fn operator_purchase(
+    &mut self,
+    ticket_buyer: &Keypair,
+    recipient: Pubkey,
+    event_registry_state: Pubkey,
+    ticket_sale_state: Pubkey,
+    event_capacity: Pubkey,
+    event_organizer: Pubkey,
+    ticket_nft_program_state: Pubkey,
+    event_id: [u8; 32],
+    ticket_type_index: u8,
+    seat_index: u32,
+		seat_name: String,
+  ) -> AnchorResult<()> {
+    let cpi_authority = TickerSalePda::cpi_authority(&ticket_sale_state).0;
+    let ticket_nft = TicketNftPda::ticket_nft(&ticket_nft_program_state, seat_index, event_id, ticket_type_index).0;
+    let event_nft = EventRegistryPda::event_nft(&event_registry_state, event_id).0;
+    let sale = TickerSalePda::ticket_sale_state(&ticket_sale_state, ticket_type_index, event_id).0;
+    let seat_verification = TickerSalePda::seat_verification(&sale, seat_index, &seat_name).0;
+
+    let accounts = ticket_sale::accounts::OperatorPurchase {
+      state: ticket_sale_state,
+      seat_verification,
+      event: EventRegistryPda::event(&event_registry_state, event_id).0,
+      sale: TickerSalePda::ticket_sale_state(&ticket_sale_state, ticket_type_index, event_id).0,
+      cpi_authority,
+      event_capacity,
+      event_organizer,
+      ticket_buyer: ticket_buyer.pubkey(),
+      ticket_nft_program_state,
+      ticket_nft,
+      nft_authority: TicketNftPda::nft_authority(&ticket_nft_program_state).0,
+      ticket_metadata: TicketNftPda::ticket_metadata(&ticket_nft_program_state, &ticket_nft).0,
+      ticket_nft_ata: Spl::get_associated_token_address(&cpi_authority, &ticket_nft),
+      event_nft,
+      event_nft_metadata: find_metadata_account(&event_nft).0,
+      ticket_nft_program: ticket_nft_program_id(),
+      token_program: Token::id(),
+      associated_token_program: spl_associated_token_account::ID,
+      system_program: system_program::ID,
+      rent: Rent::id(),
+    }.to_account_metas(None);
+
+    let data = ticket_sale::instruction::OperatorPurchase {seat_index, seat_name, recipient}.data();
+
+    let ix = Instruction {
+      program_id: ticket_sale_program_id(),
+      accounts,
+      data,
+    };
+
+    self.process_transaction(&[ix], Some(&[&ticket_buyer])).await
+  }
 }
